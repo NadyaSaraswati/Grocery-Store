c03076b96f88c1ee96389c2bd986154c55d4bcc8
NadyaSaraswati <135929497+NadyaSaraswati@users.noreply.github.com>
2023-12-14 19:00:32 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </section>
+
+</footer>
